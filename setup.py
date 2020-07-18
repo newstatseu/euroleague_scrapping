82e187150c7fafb1029b57b5efb95db9c6ad5ab0
Euroleague Dataguy <euroleague.dataguy@gmail.com>
2020-07-18 23:26:08 +0100

    added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='euroleague_scrapping',
+    version='0.1',
+    packages=['euroleague_scrapping'],
+    url='https://github.com/newstatseu/euroleague_scrapping',
+    license='Apache 2.0',
+    author='NewstatsEu',
+    author_email='euroleague.dataguy@gmail.com',
+    description='Eurocup/Euroleague Scrapping'
+)
