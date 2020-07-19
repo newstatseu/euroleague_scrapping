c456173d80322ca400c597616d6a8b75968a1693
Euroleague Dataguy <euroleague.dataguy@gmail.com>
2020-07-19 10:13:47 +0100

    build utils

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
+    setup_requires=["pbr"],
+    pbr=True,
     name='euroleague_scrapping',
-    version='0.2',
     packages=['euroleague_scrapping'],
     url='https://github.com/newstatseu/euroleague_scrapping',
     license='Apache 2.0',
