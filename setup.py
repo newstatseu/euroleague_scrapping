8981d6129302f4b6caefd7aa04e4285d978dc682
Euroleague Dataguy <euroleague.dataguy@gmail.com>
2020-07-19 11:24:57 +0100

    adding travis

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     setup_requires=["pbr"],
     pbr=True,
-    name='euroleague_scrapping',
-    packages=['euroleague_scrapping'],
-    url='https://github.com/newstatseu/euroleague_scrapping',
-    license='Apache 2.0',
-    author='NewstatsEu',
-    author_email='euroleague.dataguy@gmail.com',
-    description='Eurocup/Euroleague Scrapping',
-    long_description='Eurocup/Euroleague Scraping'
+    # name='euroleague_scrapping',
+    # packages=['euroleague_scrapping'],
+    # url='https://github.com/newstatseu/euroleague_scrapping',
+    # license='Apache 2.0',
+    # author='NewstatsEu',
+    # author_email='euroleague.dataguy@gmail.com',
+    # description='Eurocup/Euroleague Scrapping',
+    # long_description='Eurocup/Euroleague Scraping'
 )
