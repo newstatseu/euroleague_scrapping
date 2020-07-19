b9d34c8810b23364c1000180d0d2de03fd0a5e6c
Euroleague Dataguy <euroleague.dataguy@gmail.com>
2020-07-19 12:36:14 +0100

    renaming

--- euroleague_scrapping/example.py
+++ euroleague_scrapping/example.py
@@ -2,4 +2,4 @@ from euroleague_scrapping.Euroscrapper import Euroscrapper
 
 if __name__ == '__main__':
     scrapper = Euroscrapper(league="eurocup",years=[2019])
-    scrapper.start()
+    scrapper.start()impo
